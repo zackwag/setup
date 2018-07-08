83f37069102eff296018d010c5b76ca52372b887
Zack Wagner <zackwag@mac.com>
2018-07-08 13:29:50 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-# Install dotfiles
-DOTFILES_REMOTE='https://github.com/johnlim/dotfiles.git' 
-DOTFILES_LOCAL=~/git/dotfiles
-git clone $DOTFILES_REMOTE $DOTFILES_LOCAL 
-chmod +x $DOTFILES_LOCAL/setup.sh
-$DOTFILES_LOCAL/setup.sh
 
 #install NVM (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 source $HOME/.bash_profile
 nvm install node
 
-#install bower, grunt-cli, browserify and gulp 
-npm install -g bower
-npm install -g grunt-cli
-npm install -g browserify
-npm rm -g gulp
-npm i -g gulp-cli
-npm install -g polymer-cli
-npm install -g web-component-tester
-npm install -g wt-cli 
 
 # Install  Homebrew. Homebrew  is the equivalent of apt-get for linux. Its required to install tools such as git and wget.
 # Mac OS 10.9 will automatically prompt user to install XCode command line tools which is a pre-requisite.
@@ -62,81 +47,50 @@ else
 fi
 
 #Homebrew will show warning message if already installed.  
-#Install Vim
-brew install vim
-
 # Install Git
 brew install git
-
-# Install kdiff3 & set it as the default external  diff/merge too for git
-brew list | grep kdiff3
-RETSTATUS=$?
-if [ $RETSTATUS -ne 0 ]
-then
-  brew install Caskroom/cask/kdiff3
-  git config --global diff.tool kdiff3
-  git config --global merge.tool kdiff3
-  git config --global core.editor "/usr/local/bin/vim"
-else
-  tput setaf 1 && tput smul
-  echo Warning:
-  tput sgr0
-  echo kdiff3 already installed.
-fi
 
 # Install SDKMAN
 curl -s get.sdkman.io | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
+# Install JDK
 sdk install java
-
-# Install G* tools
-sdk install groovy
-sdk install grails
-sdk install gradle
 
 # Install wget
 brew install wget
 
 # Install python (comes with Setuptools and pip)
-brew install python 
-
-#install automake
-brew install automake
-
-#install libtool
-brew install libtool 
-
-#install ruby
-brew install ruby
-gem install jekyll
-gem install compass
+brew install python
 
 # Install Homebrew-cask. A CLI workflow for the adminisration of Mac applications distributed as binaries http://caskroom.io
 echo Tapping caskroom/cask...
 brew tap caskroom/cask
 brew install brew-cask 
 
-# Install IntelliJ Ulitmate
-brew cask install intellij-idea
+# Install Visual Studio Code
+brew cask install visual-studio-code
 
-# Install Alfred
-brew cask list | grep alfred 
-RETSTATUS=$?
-if [ $RETSTATUS -ne 0 ]
-then
-  brew cask install alfred 
-  brew cask alfred 
-else
-  brew cask install alfred #use brew casks' warning message
-fi
+# Install atom
+brew cask install atom
 
-# Install SequelPro
-brew cask install sequel-pro
+# Install iTerm2
+brew cask install iterm2
 
-# Install Skype
-brew cask install skype
+# Install Postman
+brew cask install postman
+
+# Install SourceTree
+brew cask install sourcetree
 
 # Install Chrome
 brew cask install google-chrome
 
+# Install IINA (VLC is outdated)
+brew cask install iina
+
+# Install The Unarchiver
+brew cask install the-unarchiver
+
+# Install Cakebrew
+brew cask install cakebrew
