94d1fa8081792cc51cd6fa1d8e5864928de70378
Zack Wagner <zackwag@mac.com>
2020-03-03 14:15:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,46 +1,5 @@
-#!/bin/bash
-
-# Install  Homebrew. Homebrew  is the equivalent of apt-get for linux. Its required to install tools such as git and wget.
-# Mac OS 10.9 will automatically prompt user to install XCode command line tools which is a pre-requisite.
-which brew
-RETSTATUS=$?
-if [ $RETSTATUS -ne 0 ]
-then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  tput setaf 1 && tput smul
-  echo Warning:
-  tput sgr0
-  echo Homebrew already installed.
-  echo Upgrading brew casks.....
-  #http://apple.stackexchange.com/questions/190072/is-there-any-way-to-upgrade-brew-caski
-  # one-liner equivalent = curl -s https://gist.githubusercontent.com/atais/9c72e469b1cbec35c7c430ce03de2a6b/raw/36808a0544628398f26b48f7a3c7b309872ca2c6/cask_upgrade.sh | bash /dev/stdin
-  (set -x; brew update;)
-
-  (set -x; brew cleanup;)
-  (set -x; brew cask cleanup;)
-
-  red=`tput setaf 1`
-  green=`tput setaf 2`
-  reset=`tput sgr0`
-
-  casks=( $(brew cask list) )
-
-  for cask in ${casks[@]}
-  do
-    version=$(brew cask info $cask | sed -n "s/$cask:\ \(.*\)/\1/p")
-    installed=$(find "/usr/local/Caskroom/$cask" -type d -maxdepth 1 -maxdepth 1 -name "$version")
-    if [[ -z $installed ]]; then
-      echo "${red}${cask}${reset} requires ${red}update${reset}."
-      (set -x; brew cask uninstall $cask --force;)
-      (set -x; brew cask install $cask --force;)
-    else
-      echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
-    fi
-  done
-fi
-
-# Homebrew will show warning message if already installed.
+# Install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install Git
 brew install git
@@ -104,7 +63,7 @@ brew cask install vanilla
 brew cask install cakebrew
 
 # Download Hyper Settings
-wget -O .hyper.js https://raw.githubusercontent.com/zackwag/setup/master/hyper.js
+wget https://gist.githubusercontent.com/zackwag/8a589bee3655355afd446d564f5a2639/raw/06a3fa995d2fce1eba8357a156faaee243a5b03a/.hyper.js
 
 # Create Empty SSH Known Hosts File
 sudo mkdir ~/.ssh/
